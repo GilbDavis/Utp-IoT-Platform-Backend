071e514cc17bda728c70815eacd7b29798fd7802
Gilberto <peter01@hotmail.es>
2020-07-25 14:17:27 -0500

    Added production database configuration

--- config/config.js
+++ config/config.js
@@ -16,10 +16,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": configEnv.database.production.db_user,
+    "password": configEnv.database.production.db_password,
+    "database": configEnv.database.production.db_dabatase,
+    "host": configEnv.database.production.db_host,
+    "dialect": configEnv.database.production.db_dialect,
+    "port": configEnv.database.production.db_port
   }
 };
--- config/configEnv.js
+++ config/configEnv.js
@@ -17,7 +17,15 @@ module.exports = {
     db_host: process.env.DB_HOST,
     db_database: process.env.DB_DATABASE,
     db_user: process.env.DB_USER,
-    db_password: process.env.DB_PASSWORD
+    db_password: process.env.DB_PASSWORD,
+    production: {
+      db_host: process.env.RDS_HOSTNAME,
+      db_dabatase: process.env.RDS_DBNAME,
+      db_user: process.env.RDS_USERNAME,
+      db_password: process.env.RDS_PASSWORD,
+      db_port: process.env.RDS_PORT,
+      db_dialect: process.env.RDS_DIALECT
+    }
   },
   mqtt_broker: process.env.BROKER
 };
