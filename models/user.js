1a02d0e22ef34cc63a9fb385983da364c347fe2c
Gilberto <peter01@hotmail.es>
2020-07-27 00:34:32 -0500

    Added a new column to database

--- models/user.js
+++ models/user.js
@@ -37,6 +37,11 @@ module.exports = (sequelize, DataTypes) => {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
     }
   }, {
     sequelize,
